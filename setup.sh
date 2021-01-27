64f357b9b9c7cbac417ec95c2a371d2c1840ce20
Matthew Wolffsohn <matthew@wolfie.ovh>
2021-01-27 20:58:58 +0000

    Mixin test

--- setup.sh
+++ setup.sh
@@ -10,6 +10,10 @@ echo "Copying new files..."
 
 cp -R /tmp/creeperpackinstaller/* /opt/minecraftftb
 
+echo "Copying Mix-ins if any..."
+
+cp - R /mixins/* /opt/minecraftftb/mods
+
 # echo "Creating Minecraft EULA file"
 echo "Now starting server 👈😎👈"
 echo "eula=true" > eula.txt
